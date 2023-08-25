6157c30ccb2940082c64192c4a8d3e30262d2b28
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-25 22:29:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 //your JS code here. If required.
-let API_KEY = "9e632c24fc993f6c55c5949921d62ef6";
-let baseUrl = `https://api.openweathermap.org/data/2.5/`;
+
 
 const button = document.getElementById("btn");
 const container = document.getElementById("container");
 
 button.addEventListener("click",() => {
-    let fetchString = "London";
-    
-    getFetchResult(fetchString);
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de')
+         .then(response => response.json())
+	.then(data => {
+         container.innerHTML += `<div>Current weather in London:${data.weather[0].main}</div>`;;
+         })
+
+	container.appendChild(div);
 })
 
-async function getFetchResult(fetchString){
-    let url = `${baseUrl}/weather?&q=${fetchString}&appid=${API_KEY}&units=metric`
-    console.log(url);
-    const response = await fetch(url, {method:"GET"});
-    const result = await response.json();
-	let div = document.createElement("div");
-
-	div.innerHTML = `<div>Current weather in London:${result.weather[0].main}</div>`;
-	container.appendChild(div);
-    
-}
