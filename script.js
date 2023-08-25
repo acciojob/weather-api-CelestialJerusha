0db855e64dd003df6b8af4481fb67e029253d5a4
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-25 22:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ button.addEventListener("click",() => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de')
          .then(response => response.json())
 	.then(data => {
-         container.innerHTML += `<div>Current weather in London:${data.weather[0].main}</div>`;;
+         container.innerHTML += `<div>Current weather in London: ${data.weather[0].main}</div>`;;
          })
 
 	//container.appendChild(div);
